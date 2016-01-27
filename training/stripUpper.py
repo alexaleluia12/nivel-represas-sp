af6719d4421012990fc03f5ddc7d27703e93946a
alexaleluia12 <alexaleluiaforgit@gmail.com>
2016-01-27 21:21:53 -0200

    corrigi bug

--- training/stripUpper.py
+++ training/stripUpper.py
@@ -1,59 +1,84 @@
-#!/usr/bin/env python
 #-*- coding:utf-8 -*-
+from __future__ import print_function
 
 import re
 
-"""
-as duas funcoes abaixo fazer a mesma coisa de forma diferente
-
-a 'splitUpper' faz de uma maneira mais robusta
-"""
+regex = re.compile(r'[A-Z]')
 
 def splitUpper(tarString):
-    """ 
-        tarString: string que sera esplitada
-        
-        --
-        retorna um lista de string, mas nao esclui os caraceter da Match,
-        e descondidera a primeria letra da string, usa letra maiusculas como referencia
-    """
     listElements = []
-    stRegular = r'[A-Z]'
-    allUpper = re.findall(stRegular, tarString)    
-    assert allUpper, "A string deve ter mais de uma letra maiuscula"
-    last = allUpper[-1]
-    firstChar = tarString[0]
-    for letra in allUpper:  
-        if firstChar == letra:
-            continue     
-        indice = tarString.find(letra)     
-        listElements.append(tarString[:indice])
-        tarString = tarString[indice:]# a ultima sai aqui, entao adiciona depois do for        
-    listElements.append(tarString)
+    scanner = regex.finditer(tarString)
+    end = 0
+    start = 0
+    
+    for match in scanner:
+        if match.start() == 0:# pula o primeiro
+            continue
+        end = match.start()
+        listElements.append(tarString[start:end])
+        start = end
+        # ainda resta mais uma string p/ adicionar depois do for
+    listElements.append(tarString[start:])
+    
     return listElements
-    
-        
-    
 
-def inserirSpaco(st):
-    def spaco(stInner, indice):
-        assert(indice >= 0)
-        return stInner[:indice] + " " + stInner[indice:]
-        
-    allUpper = re.findall(r'[A-Z]', st)
-    print(allUpper)
-    if not allUpper:
-        return st
-    for letra in allUpper:
-        print(st)
-        st = spaco(st, st.find(letra))
-    return st
+
+
+def ndivide(st):
+    new_st = st[1:]
+    scanner = regex.finditer(new_st)
     
+    match = next(scanner)
+    local_start = match.start() + 1
+    local_end = 0
+    yield st[:local_start]
     
-        
+    local_start -= 1
+    for i in scanner:
+        local_end = i.start()
+        yield new_st[local_start: local_end]
+        local_start = local_end
     
+    yield new_st[local_start:]
+
+
+
+
 # SaoCarlosDaBara
 
-if __name__ == "__main__":
-    print('$'.join(splitUpper("SaoCarlosDaBara")))    
+## mediar performace
+def test_ndivide(st):
+    print(' '.join(ndivide(st)))
 
+def test_splitUpper(st):
+    print(' '.join(splitUpper(st)))
+
+# resultado
+"""
+test_ndivide
+
+Sao Carlos Da Bara E Uma Cidade Muito Bonita Para Se Ver
+
+real	0m0.019s
+user	0m0.015s
+sys	0m0.004s
+
+
+
+
+###
+test_splitUpper
+
+Sao Carlos Da Bara E Uma Cidade Muito Bonita Para Se Ver
+
+real	0m0.020s
+user	0m0.016s
+sys	0m0.004s
+
+
+"""
+
+
+if __name__ == "__main__":
+    str_test = "SaoCarlosDaBaraEUmaCidadeMuitoBonitaParaSeVer"
+    test_ndivide(str_test)
